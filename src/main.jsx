07a5a359b4dc073c08dbb3031d263a46edcdb2b1
Logisolve <nikz.250@gmail.com>
2026-05-18 22:03:40 -0600

    fix: dark background in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
 
+document.body.style.margin = '0'
+document.body.style.padding = '0'
+document.body.style.background = '#070909'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
